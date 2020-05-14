aeaebe512a3fdf2665ac9f6c838c35ff504cc145
정병화 <bhjung@nubes-bridge.com>
2020-05-14 17:57:52 +0900

    add withCredentials to axios config for cookies

--- src/shared/components/login/LoginCard.jsx
+++ src/shared/components/login/LoginCard.jsx
@@ -8,8 +8,8 @@ const LoginCard = () => {
             <div className="account__card">
                 <div className="account__head">
                     <h3 className="account__title">Welcome to
-                        <span className="account__logo"> Contents
-                            <span className="account__logo-accent"> Bridge</span>
+                        <span className="account__logo"> 콘텐츠
+                            <span className="account__logo-accent"> 브릿지</span>
                         </span>
                     </h3>
                     <h4 className="account__subhead subhead">통합플랫폼</h4>
