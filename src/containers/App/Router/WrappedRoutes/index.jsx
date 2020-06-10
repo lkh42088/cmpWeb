a81da7b9784c251937a7d8ae5332d61c43265eef
정병화 <bhjung@nubes-bridge.com>
2020-06-10 09:34:59 +0900

    원본 템플릿 복구

--- src/containers/App/Router/WrappedRoutes/index.jsx
+++ src/containers/App/Router/WrappedRoutes/index.jsx
@@ -27,24 +27,24 @@ export default () => (
     <div>
         <Layout />
         <div className="container__wrap">
-            <Route path="/dashboard_default" component={DefaultDashboard} />
-            <Route path="/dashboard_e_commerce" component={Commerce} />
-            <Route path="/dashboard_fitness" component={FitnessDashboard} />
-            <Route path="/dashboard_crypto" component={Crypto} />
-            <Route exact path="/dashboard_mobile_app" component={MobileAppDashboard} />
-            <Route path="/dashboard_booking" component={BookingDashboard} />
-            <Route path="/ui" component={UI} />
-            <Route path="/mail" component={Mail} />
-            <Route path="/chat" component={Chat} />
-            <Route path="/todo" component={Todo} />
-            <Route path="/forms" component={Forms} />
-            <Route path="/tables" component={Tables} />
-            <Route path="/charts" component={Charts} />
-            <Route path="/maps" component={Maps} />
-            <Route path="/account" component={Account} />
-            <Route path="/e-commerce" component={ECommerce} />
-            <Route path="/default_pages" component={DefaultPages} />
-            <Route path="/documentation" component={Documentation} />
+            <Route path="/org/dashboard_default" component={DefaultDashboard} />
+            <Route path="/org/dashboard_e_commerce" component={Commerce} />
+            <Route path="/org/dashboard_fitness" component={FitnessDashboard} />
+            <Route path="/org/dashboard_crypto" component={Crypto} />
+            <Route exact path="/org/dashboard_mobile_app" component={MobileAppDashboard} />
+            <Route path="/org/dashboard_booking" component={BookingDashboard} />
+            <Route path="/org/ui" component={UI} />
+            <Route path="/org/mail" component={Mail} />
+            <Route path="/org/chat" component={Chat} />
+            <Route path="/org/todo" component={Todo} />
+            <Route path="/org/forms" component={Forms} />
+            <Route path="/org/tables" component={Tables} />
+            <Route path="/org/charts" component={Charts} />
+            <Route path="/org/maps" component={Maps} />
+            <Route path="/org/account" component={Account} />
+            <Route path="/org/e-commerce" component={ECommerce} />
+            <Route path="/org/default_pages" component={DefaultPages} />
+            <Route path="/org/documentation" component={Documentation} />
         </div>
     </div>
 );
