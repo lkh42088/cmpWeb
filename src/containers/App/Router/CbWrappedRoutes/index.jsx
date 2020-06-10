bc8dacd01a92ba7f1ac28af1f626d4574726a5ec
khlee <khlee@nubes-bridge.com>
2020-06-10 15:28:55 +0900

    Add to Subnet table in router.

--- src/containers/App/Router/CbWrappedRoutes/index.jsx
+++ src/containers/App/Router/CbWrappedRoutes/index.jsx
@@ -6,6 +6,7 @@ import Assets from './Assets';
 import Customer from './Customer';
 import Management from "./Management";
 import DefaultPages from './DefaultPages';
+import Subnet from "./Subnet";
 
 export default () => (
     <div>
@@ -14,6 +15,7 @@ export default () => (
             <Route path="/assets" component={Assets}/>
             <Route path="/customers" component={Customer}/>
             <Route path="/management" component={Management}/>
+            <Route path="/subnet/write" component={Subnet}/>
             <Route path="/default_pages" component={DefaultPages}/>
             <Route path="/404" component={DefaultPages}/>
         </div>
