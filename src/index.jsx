31062cacb629684397f4b3de1d3ba7adec16d58e
khlee <khlee@nubes-bridge.com>
2020-08-10 15:05:33 +0900

    사파리 브라우저를 위한 테스트 코드 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,6 @@
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/stable';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
