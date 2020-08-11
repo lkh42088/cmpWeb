c29003d1ac7619b9f44ecfc44918d35fdaf07a6d
khlee <khlee@nubes-bridge.com>
2020-08-11 19:43:12 +0900

    VNC 기능을 위한 코드 추가

--- src/containers/MicroCloud/Vnc/components/VncDisplayPanel.jsx
+++ src/containers/MicroCloud/Vnc/components/VncDisplayPanel.jsx
@@ -1,32 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {TextField} from "@material-ui/core";
 import VncDisplay from "react-vnc-display";
 
-const VncDisplayPanel = () => {
-    const [url, setUrl] = useState('');
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        console.log("■■■ ", e.target.value);
-        if (e.target.value) {
-            setUrl(e.target.value);
-        }
-    };
+const VncDisplayPanel = (props) => {
+    const {target, port} = props;
 
     return (
         <>
-            <form>
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="VNC URL"
-                    rowsMax={4}
-                    value={url}
-                    onChange={handleChange}
-                    variant="outlined"
-                />
-            </form>
-            {/*<VncDisplay />*/}
-            <VncDisplay url="wss://192.168.0.73:5900" />
+            <VncDisplay url="ws://127.0.0.1:5000" />
         </>
     );
 };
