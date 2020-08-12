a2ecca4c7ef60aab4bf639181e073023aaf43146
khlee <khlee@nubes-bridge.com>
2020-08-12 09:43:32 +0900

    IE 를 위한 가이드 페이지 구현 중

--- src/containers/App/Router/index.jsx
+++ src/containers/App/Router/index.jsx
@@ -6,29 +6,21 @@ import CbWrappedRoutes from './CbWrappedRoutes';
 import MicroWrappedRoutes from './MicroWrappedRoutes';
 import LogInPage from "../../Login/LogInPage";
 import LogInEmailAuth from "../../Login/AuthFromEmail";
-import EdgeLaunch, {CheckIE} from "../../Common/BrowserCheck";
+import EdgeLaunch, {CheckIE, GuidePage} from "../../Common/BrowserCheck";
 
-const Router = () => {
-    /** IE is right, Open 1.Edge 2.Chrome */
-    if (CheckIE() === true) {
-        EdgeLaunch();
-    } else {
-        return (
-            <MainWrapper>
-                <main>
-                    <Switch>
-                        <Route exact path="/" component={LogInPage}/>
-                        <Route path="/login/email/:id/:target/:secret" component={LogInEmailAuth}/>
-                        <Route path="/login" component={LogInPage}/>
-                        <Route path="/org" component={WrappedRoutes}/>
-                        <Route path="/micro" component={MicroWrappedRoutes}/>
-                        <Route path="/" component={CbWrappedRoutes}/>
-                    </Switch>
-                </main>
-            </MainWrapper>
-        );
-    }
-    return null;
-};
+const Router = () => (
+        <MainWrapper>
+            <main>
+                <Switch>
+                    <Route exact path="/" component={LogInPage}/>
+                    <Route path="/login/email/:id/:target/:secret" component={LogInEmailAuth}/>
+                    <Route path="/login" component={LogInPage}/>
+                    <Route path="/org" component={WrappedRoutes}/>
+                    <Route path="/micro" component={MicroWrappedRoutes}/>
+                    <Route path="/" component={CbWrappedRoutes}/>
+                </Switch>
+            </main>
+        </MainWrapper>
+    );
 
 export default Router;
