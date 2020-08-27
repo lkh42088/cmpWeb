9152a8eb6a8882702d42446327ed90ca06415efd
khlee <khlee@nubes-bridge.com>
2020-08-27 13:57:09 +0900

    VM traffic monitoring code

--- src/containers/Dashboards/MicroCloud/components/MyResponsiveLine.jsx
+++ src/containers/Dashboards/MicroCloud/components/MyResponsiveLine.jsx
@@ -4,28 +4,28 @@ import {Card, CardBody} from "reactstrap";
 import {readSubnet} from "../../../../lib/api/subnet";
 import {getVmInterfaceTraffic} from "../../../../lib/api/microCloud";
 
-const data = [
+const data1 = [
     {
         id: "RX",
         data: [
             {
-                x: "20:00",
+                x: "20:00:00",
                 y: 596685,
             },
             {
-                x: "21:00",
+                x: "21:00:00",
                 y: 592929,
             },
             {
-                x: "22:00",
+                x: "22:00:00",
                 y: 394994,
             },
             {
-                x: "23:00",
+                x: "23:00:00",
                 y: 494944,
             },
             {
-                x: "24:00",
+                x: "24:00:00",
                 y: 4938494,
             },
         ],
@@ -34,23 +34,23 @@ const data = [
         id: "TX",
         data: [
             {
-                x: "20:00",
+                x: "20:00:00",
                 y: 1829839,
             },
             {
-                x: "21:00",
+                x: "21:00:00",
                 y: 629348,
             },
             {
-                x: "22:00",
+                x: "22:00:00",
                 y: 3298843,
             },
             {
-                x: "23:00",
+                x: "23:00:00",
                 y: 939494,
             },
             {
-                x: "24:00",
+                x: "24:00:00",
                 y: 3294393,
             },
         ],
@@ -62,65 +62,91 @@ const MyResponsiveLine = (props) => {
         title, height, mac,
     } = props;
 
-    const [state, setState] = useState({
-        id: "RX",
-        data: [{
-            x: Date(),
-            y: 0,
-        }],
+    const [data, setData] = useState({
+        stats: [{
+                id: "RX",
+                data: [{ x: new Date("00:00:00"), y: 0 }],
+            }, {
+                id: "TX",
+                data: [{ x: new Date("00:00:00"), y: 0 }],
+            },
+        ],
     });
-    // const [state, setState] = useState([{
-    //     time: null,
-    //     hostname: "",
-    //     ifDescr: "",
-    //     ifPhysAddress: "",
-    //     ifInOctets: 0,
-    //     ifOutOctets: 0,
-    // }]);
+    const [hostname, setHostname] = useState("");
 
     /**************************************************************
      * Axios Function
      **************************************************************/
     const getData = async () => {
         // console.log("★★★★★★★★", "get Interface Traffic! mac:", mac);
+        if (!mac) {
+            return;
+        }
+
         try {
             const response = await getVmInterfaceTraffic({mac});
-            setState({
-                data: (
-                    response.data.stats.map(val => ({
-                        x: Date.parse(val.time),
-                        y: val.ifInOctets,
-                    }))
-                ),
+            const rxData = data.slice(1);
+            const txData = data.slice(2);
+            rxData.push({
+                // eslint-disable-next-line no-undef
+                x: time.timeMinute.offset(response.data.stats[0].data[0].x, 30),
+                y: 10 + Math.round(Math.random() * 20),
             });
-            console.log(state.stats);
+            txData.push({
+                // eslint-disable-next-line no-undef
+                x: time.timeMinute.offset(response.data.stats[0].data[0].x, 30),
+                y: 10 + Math.round(Math.random() * 20),
+            });
+            // console.log("TEST RESPONSE1: ", response.data.stats[0].data);
+            // setData({
+            //     stats: (
+            //         response.data.stats.map(val => ({
+            //             id: val.id,
+            //             data: val.data.map(s => ({
+            //                 ...s,
+            //                 // x: s.x,
+            //                 // y: s.y,
+            //             })),
+            //         }))),
+            // });
+            setData({rxData, txData});
+            setHostname(response.data.hostname);
+            console.log("TEST RESPONSE2: ", hostname);
+            console.log("TEST RESPONSE3: ", data.stats);
         } catch {
-            setState({
-                ...state,
+            setData({
+                ...data,
                 data: [],
             });
         }
     };
 
+    const test = () => {
+        console.log(data.stats);
+    };
+
     useEffect(() => {
-        if (mac) {
-            getData();
-        }
+
+    }, [data.stats]);
+
+    useEffect(() => {
+        // const timer = setInterval(getData, 3000);
+        getData();
     }, []);
 
     return (
         <Card className="cb-card">
             <CardBody className="cb-card-body">
-                <p>{title}</p>
+                <p>{title} <b>({hostname})</b></p>
                 <ResponsiveLine
-                    data={state.data}
+                    data={data1}
                     height={height}
                     margin={{
                         top: 50, right: 110, bottom: 50, left: 60,
                     }}
-                    xScale={{ type: 'linear' }}
+                    xScale={{ type: 'time', format: 'native' }}
                     yScale={{
-                        type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false,
+                        type: 'linear', min: 'auto', max: 'auto', stacked: false, reverse: false,
                     }}
                     curve="monotoneX"
                     axisTop={null}
@@ -133,7 +159,7 @@ const MyResponsiveLine = (props) => {
                         legend: 'transportation',
                         legendOffset: 36,
                         legendPosition: 'middle',
-                        // format: "%H:%M",
+                        format: "%H:%M:%S",
                         // tickValue: "every 5 minutes",
                     }}
                     axisLeft={{
@@ -157,6 +183,7 @@ const MyResponsiveLine = (props) => {
                     enableArea
                     areaOpacity={0.15}
                     useMesh
+                    enableSlices="x" //todo
                     legends={[
                         {
                             anchor: 'bottom-right',
@@ -184,6 +211,7 @@ const MyResponsiveLine = (props) => {
                         },
                     ]}
                     motionStiffness={100}
+                    onClick={test}
                 />
             </CardBody>
         </Card>
